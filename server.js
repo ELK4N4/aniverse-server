5a2eca32dc21f1fb70b7c00083cc0a1b5d648b72
moish kev <moishi67@gmail.com>
2021-08-19 21:26:19 +0300

    added request limiter for production

--- server.js
+++ server.js
@@ -17,22 +17,38 @@ import animesRouter from './routes/animes.js';
 import loginRouter from './routes/login.js';
 import authRouter from './routes/auth/auth.js';
 import userRouter from './routes/user.js';
+import rateLimit  from "express-rate-limit";
 
-const app = express();
 dotenv.config();
 
+// Enable if you're behind a reverse proxy (Heroku, Bluemix, AWS ELB, Nginx, etc)
+// see https://expressjs.com/en/guide/behind-proxies.html
+// app.set('trust proxy', 1);
+
+
 /* Constant Variables */
+const app = express();
 const PORT = process.env.PORT || 5000;
 const DATABASE_URL = process.env.DATABASE_URL || "mongodb://localhost/test";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    max: 100 // limit each IP to 100 requests per windowMs
+});
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isProduction = process.env.NODE_ENV === 'production';
 
 /* Server Setup */
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout', 'layouts/layout');
 
+
+if(isProduction)
+{
+    app.use(limiter);
+}
 app.use(helemt());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
