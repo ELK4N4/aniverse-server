50a40504e36f03bdc2dab1a47c92b62edb5d9afa
moish kev <moishi67@gmail.com>
2021-09-19 01:49:58 +0300

    unfollow

--- routes/fansubs/followers.js
+++ routes/fansubs/followers.js
@@ -21,4 +21,20 @@ router.postAsync('/', async (req, res) => {
     res.status(203).send('Following successfully');
 });
 
+//DELETE follower
+router.deleteAsync('/', async (req, res) => {
+    if(!req.user.followingFansubs.includes(req.fansub._id)){
+        return res.status(401).send('You are not following');
+    }
+
+    var index = req.user.followingFansubs.indexOf(req.fansub._id);
+    req.user.followingFansubs.splice(index, 1);
+    await req.user.save();
+    
+    req.fansub.followers--;
+    await req.fansub.save();
+
+    res.status(203).send('Unfollowing successfully');
+});
+
 export default router;
