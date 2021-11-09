3b0b729899dc73880b3558101f7f61d882fb59a9
ELK4N4 <elkana546@gmail.com>
2021-11-09 14:36:20 +0200

    added validation to episodes

--- routes/fansubs/episodes.js
+++ routes/fansubs/episodes.js
@@ -1,4 +1,6 @@
+import { episodeScheme } from '@aniverse/utils/validations/index.js';
 import { Router } from '@awaitjs/express';
+import validate from '../../middlewares/validation.js';
 import Episode from '../../models/Episode.js';
 
 const router = Router();
@@ -20,7 +22,7 @@ router.getAsync('/:episodeId', async (req, res) => {
 });
 
 //POST new episodes
-router.postAsync('/', async (req, res) => {
+router.postAsync('/', validate(episodeScheme), async (req, res) => {
     // TODO add check if user member in fansubId...
     const episodeExist = await Episode.findOne({number: req.body.number, project: req.project._id});
     if(episodeExist) {
@@ -44,7 +46,7 @@ router.postAsync('/', async (req, res) => {
 });
 
 //UPDATE exist animes
-router.putAsync('/:episodeId', async (req, res) => {
+router.putAsync('/:episodeId', validate(episodeScheme), async (req, res) => {
     const episodeId = req.params.episodeId;
 
     const oldEpisode = await Episode.findById(episodeId);
