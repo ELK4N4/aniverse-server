6da0cc5bf3460c633cf7a6af45e126bbff67b1d7
ELK4N4 <elkana546@gmail.com>
2022-03-06 23:57:11 +0200

    added about to users

--- models/User.js
+++ models/User.js
@@ -30,6 +30,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: ''
     },
+    about: {
+        type: String,
+        default: ''
+    },
     memberInFansubs: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Fansub'
